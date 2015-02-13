096212f6f5f437ce05b431182141ab57a3061189
Дмитрий <todorovskiy_dima@yahoo.com>
2015-02-13 16:59:10 +0200

    Update mainwindow.cpp

--- OpenBoard/mainwindow.cpp
+++ OpenBoard/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
