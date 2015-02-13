297fd640ea148c4b56437dcc7f43269da2e24c25
Andrii N <enemy.vin@gmail.com>
2015-02-14 00:02:35 +0200

    add toolBar. Add comment's

--- OpenBoard/mainwindow.cpp
+++ OpenBoard/mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //fixed toolBar
+    ui->mainToolBar->setMovable(false);
+    //enable to hide toolBar (rightMouseClick)
+    ui->mainToolBar->setContextMenuPolicy(Qt::CustomContextMenu);
 }
 
 MainWindow::~MainWindow()
@@ -14,7 +18,7 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::on_action_Font_triggered()
-{
+{   //call QtFontDialog & setFont to all elements (this)
     bool ok;
     QFont font;
     font = QFontDialog::getFont(&ok, QFont( "Times", 10 ), this);
@@ -24,12 +28,12 @@ void MainWindow::on_action_Font_triggered()
 }
 
 void MainWindow::on_action_Reset_default_triggered()
-{
+{   //set font to menuBar: Times, 8 pt, normal
     ui->menuBar->setFont(QFont("Times",8,1,false));
 }
 
 void MainWindow::on_action_Color_triggered()
-{
+{   //call QtColorDialog
     QColor colorm;
     colorm = QColorDialog::getColor();
 }
