d211b060895df5e68cac886fbf0fc46067b6ddce
roma <zigzagforever@mail.ru>
2015-07-08 14:58:39 +0300

    functional keys handling updated in keylogger.

--- OpenBoard/editWidget/keyloggertextedit.cpp
+++ OpenBoard/editWidget/keyloggertextedit.cpp
@@ -14,11 +14,14 @@ void KeyloggerTE::keyPressEvent(QKeyEvent *event){
     QString keyChar(event->text());
    // QTextCursor
            int localCursorPosition = textCursor().position();
+           int localCursorPositionInBlock = textCursor().positionInBlock();
+           int localCursorBlockNumber = textCursor().blockNumber();
            int localCursorSelectionStart=textCursor().selectionStart();
            int localCursorSelectionEnd=textCursor().selectionEnd();
     QTextEdit::keyPressEvent(event);
 
             QString textInField = destination->toPlainText();
+             QStringList stringsInCommandTextEdit = toPlainText().split('\n');
     //Якщо буква то просто вставляєм її в текст
     switch(keyCode){
     case Qt::Key_Up:
@@ -36,6 +39,30 @@ void KeyloggerTE::keyPressEvent(QKeyEvent *event){
         qDebug() << "on_KEY_RIGHT";
         if (textCursor().position()!=previousCursorPosition)
         textInField +="\\mr001";
+        break;
+    case Qt::Key_Home:
+        if (localCursorPositionInBlock!=0)
+        {
+            int delta = localCursorPositionInBlock;
+             textInField +=QString("\\ml%1").arg(delta, 3, 10, QChar('0'));
+        }
+        break;
+    case Qt::Key_End:
+
+        if (localCursorPositionInBlock!=stringsInCommandTextEdit[localCursorBlockNumber].length())
+        {
+            int delta = stringsInCommandTextEdit[localCursorBlockNumber].length()-localCursorPositionInBlock;
+             textInField +=QString("\\mr%1").arg(delta, 3, 10, QChar('0'));
+        }
+        break;
+    case Qt::Key_Delete:
+        if (localCursorSelectionEnd-localCursorSelectionStart!=0)
+        {
+            int delta = localCursorSelectionEnd-localCursorSelectionStart;
+            if (localCursorPosition==localCursorSelectionEnd)
+            textInField +=QString("\\dl%1").arg(delta, 3, 10, QChar('0'));
+            else textInField +=QString("\\dr%1").arg(delta, 3, 10, QChar('0'));
+        }
         break;
     case Qt::Key_Backspace:
         qDebug() << "on_KEY_BACKSPACE";
