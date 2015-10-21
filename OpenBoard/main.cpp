3eb83756a603999fb95c593847e35fad308a8b30
Самоенко Юрий <krdcl@mail.ru>
2015-10-21 12:31:31 +0300

    fix crash when start program

--- OpenBoard/main.cpp
+++ OpenBoard/main.cpp
@@ -20,64 +20,58 @@ QString log_time = QString("log ") + QString::number( cut_time.hour()) + QString
 void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
     QByteArray localMsg = msg.toLocal8Bit();
-   // FILE *file = fopen(DEBUG_TO_FILE, "a");
+    // FILE *file = fopen(DEBUG_TO_FILE, "a");
 
     QFile file(log_time);
-
-    char * stream = new char[500];
-    //FILE *file = fopen(DEBUG_TO_FILE, "a");
     if(file.open(QIODevice::Append)  )//file != NULL)
     {
         switch (type) {
         case QtDebugMsg:
-            sprintf(stream, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-           // fprintf(file, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            // sprintf(stream, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            file.write("Debug: ");
+            // fprintf(file, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
             printf("%s \n", localMsg.constData());
             break;
         case QtWarningMsg:
-            sprintf(stream, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-          //  fprintf(file, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            file.write("Warning: ");
+            // sprintf(stream, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            //  fprintf(file, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
             printf("%s \n", localMsg.constData());
             break;
         case QtCriticalMsg:
-            sprintf(stream, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-          //  fprintf(file, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            file.write("Critical: ");
+            //sprintf(stream, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            //  fprintf(file, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
             printf("%s \n", localMsg.constData());
             break;
         case QtFatalMsg:
-            sprintf(stream, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-          //  fprintf(file, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            file.write("Fatal: ");
+            // sprintf(stream, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            //  fprintf(file, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
             printf("%s \n", localMsg.constData());
             abort();
         default:
-           // fprintf(file, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            file.write("Info: ");
+            // sprintf(stream, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+            // fprintf(file, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
             printf("%s \n", localMsg.constData());
             break;
         }
-    file.write(stream);
-    file.write("\n");
-    delete stream;
-    file.close();
+        file.write( localMsg.constData());
+        file.write( "(" );
+        file.write( context.file );
+        file.write( ":" );
+        file.write( std::to_string(context.line).c_str() );
+        file.write( ", " );
+        file.write( context.function );
+        file.write( ")\n");
+        file.close();
     }
-    //fclose(file);
-
 }
 #endif
 
 int main(int argc, char *argv[])
 {
-    //  qApp->addLibraryPath("\libs");
-    //   qApp->addLibraryPath("/plugins");
-#ifdef DEBUG_TO_FILE
-   // DEBUG_TO_FILE = QString("log.txt" + QDateTime::currentDateTime());
-    qInstallMessageHandler(myMessageOutput);
-    QFile file (log_time);
-    file.open( QIODevice::WriteOnly);
-    file.write("Begin log:\n");
-    file.close();
-            //fopen(deb_file, "w");
-    //fclose(file);
-#endif
     QString executable = argv[0];
     QString executablePath = executable.mid(0,executable.lastIndexOf("\\"));
     QString installPathPlugins = QLibraryInfo::location(QLibraryInfo::PluginsPath);
@@ -94,13 +88,20 @@ int main(int argc, char *argv[])
     QCoreApplication::setLibraryPaths(paths);
     //    QCoreApplication::addLibraryPath("SSL");
 
+#ifdef DEBUG_TO_FILE
+    qInstallMessageHandler(myMessageOutput);
+    QFile file (log_time);
+    file.open( QIODevice::WriteOnly);
+    file.write("Begin log:\n");
+    file.close();
+#endif
 
     QApplication app(argc, argv);
 
     MainWindow window;
     window.resize(500, 500);
 #ifdef DEBUG_TO_FILE
-    window.setLogFileName(QString("log.txt"));
+    window.setLogFileName(QString(DEBUG_TO_FILE));
     window.setLogFileDateName(log_time);
 #endif
     // app.addLibraryPath("/SSL");
